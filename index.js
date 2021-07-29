efc63b33a6fdfaea0a884166eca419471de1d56f
macktrain <82425068+macktrain@users.noreply.github.com>
2021-07-28 20:26:42 -0600

    updated major functionality

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-  
-    // Port using
+    //Port
     port: 3306,
-  
     // DB username
     user: 'root',
-  
     // DB Password
     password: 'myp4wd',
-
     // DB from mysql
     database: 'hr_db',
   });
@@ -43,11 +39,16 @@ const administer_db = () => {
         choices: 
         [
             'View All Employees',
-            'View All Employees by Department',
-            'View All Employees by Manager',
             'Add New Employee',
             'Remove Employee',
             'View All Roles',
+            'Add New Role',
+            'Delete a Role',
+            'View All Departments',
+            'Add New Department',
+            'Delete a Department',
+            'View All Employees by Department',
+            'View All Employees by Manager',
             'Done',
         ]
       })
@@ -56,26 +57,43 @@ const administer_db = () => {
             case 'View All Employees':
                 viewEmps('all');
                 break;
+            case 'Add New Employee':
+                addEmp();
+                break;
+            case 'Delete a Employee':
+                deleteEmp();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'Add New Role':
+                addRole();
+                break;
+            case 'Delete a Role':
+                deleteRole();
+                break;
+            case 'View All Departments':
+                viewDepts();
+                break;
+            case 'Add New Department':
+                addDept();
+                break;
+            case 'Delete a Department':
+                deleteDept();
+                break;
             case 'View All Employees by Department':
                 viewEmpsByDept();
                 return;
             case 'View All Employees by Manager':
                 viewEmpsByMgr();
                 return;
-            case 'Add New Employee':
-                addEmp();
-                break;
-            case 'Remove Employee':
-                deleteEmp();
-                break;
-            case 'View All Roles':
-                addInternDetails();
-                break;
             case 'Done':
-                addInternDetails();
+                console.log('Goodbye.')
+                connection.end();
                 break;
             default:
                 console.log(`Please select an option`);
+                break;
         }
     });
 };
@@ -283,6 +301,240 @@ const addEmp = () => {
                     administer_db();
                 });
             });
+        });
+    });
+}
+
+const deleteEmp = () => {
+    //Query all employees for the user to select from
+    connection.query(empQuery, function (err, result, fields) {
+        if (err) throw err;
+        let empArr = [];
+        Object.keys(result).forEach(function(key) {
+            let row = result[key];
+            const emp = new Employee (row.Id, row.fName, row.lName, row.title, mgrName);
+            empArr.push(`Employee#: ${row.Id} Name: ${row.fName} ${row.lName}`);
+        });
+
+        //Now determine which employee to delete
+        inquirer.prompt({
+            name: 'toDelete',
+            type: 'list',
+            message: 'Select an employee to be deleted:',
+            choices: empArr
+        })
+        .then((answer) => {
+            let empString = answer.toDelete;
+            const empArr = empString.split(" ");
+            const empDelete = empArr[1];
+            //Now delete the selected employee
+            
+            let delQuery = `DELETE FROM employee WHERE empId = ?`;
+
+            connection.query(delQuery, [empDelete], function (err, result, fields) {
+                if (err) throw err;
+                console.log (`${empArr[3]} ${empArr[4]} with Employee# ${empArr[1]} has been ermoved.`);
+                //START OVER
+                administer_db();
+            });
+        });
+    });
+}
+
+const viewRoles = () => {
+    //Query all employees for the user to select from
+    let roleQuery = 'SELECT r.roleId, r.title, r.max_salary, d.deptId, d.deptName ';
+    roleQuery += 'FROM role AS r ';
+    roleQuery += 'JOIN department AS d ON r.deptId = d.deptId ';
+    roleQuery += 'ORDER BY r.roleId ';
+
+    connection.query(roleQuery, function (err, result, fields) {
+        if (err) throw err;
+        let roleArr = [];
+        Object.keys(result).forEach(function(key) {
+            let row = result[key];
+            roleArr.push(row);
+        });
+        console.table (roleArr)
+        //START OVER
+        administer_db();
+    });
+}
+
+const addRole = () => {
+    //Select all Departments
+    let deptQuery = 'SELECT * from department';
+    
+    //This builds out the array of depts to be used while adding a new role
+    connection.query(deptQuery, function (err, result, fields) {
+        if (err) throw err;
+        let deptArr = [];
+        Object.keys(result).forEach(function(key) {
+            let row = result[key];
+            deptArr.push(`Dept Id#: ${row.deptId} Dept Name: ${row.deptName}`);
+        });
+        
+        inquirer
+        .prompt([
+            {
+                name: 'title',
+                type: 'input',
+                message: 'What is the new role title?',
+                validate: async (input) => {
+                    if (input.length === 0) 
+                    {
+                        return 'You must enter something!';
+                    }
+                    return true;
+                }
+            },
+            {
+                name: 'max_salary',
+                type: 'input',
+                message: 'What is the new role salary cap?',
+                validate: async (input) => {
+                    if (input.length === 0)
+                    {
+                        return 'You must enter something!';
+                    }
+                    return true;
+                }
+            },
+            {
+                name: 'deptId',
+                type: 'list',
+                message: 'Which department is associated with this role?',
+                choices: deptArr
+            },
+        ])
+        .then((answer) => {
+            let deptString = answer.deptId;
+            const deptChosen = deptString.split(" ");
+            const choiceId = deptChosen[2];
+
+            let insertSQL = 'INSERT INTO role (title, max_salary, deptId) ';
+            insertSQL += `VALUES  ('${answer.title}','${answer.max_salary}',${choiceId})`;
+            
+            connection.query(insertSQL, function (err, result, fields) {
+                if (err) throw err;
+                console.log (`${answer.title} has been added.`);
+                //START OVER
+                administer_db();
+            });
+        });
+    });
+}
+
+const deleteRole = () => {
+    //Query all employees for the user to select from
+    let roleDelQuery = 'SELECT * FROM role';
+    connection.query(roleDelQuery, function (err, result, fields) {
+        if (err) throw err;
+        let roleArray = [];
+        Object.keys(result).forEach(function(key) {
+            let row = result[key];
+            roleArray.push(`RoleId#: ${row.roleId} Name: ${row.title}`);
+        });
+
+        //Now determine which employee to delete
+        inquirer.prompt({
+            name: 'toDelete',
+            type: 'list',
+            message: 'Select a role to delete:',
+            choices: roleArray
+        })
+        .then((answer) => {
+            let roleString = answer.toDelete;
+            const delStr = roleString.split(" ");
+            //Now delete the selected employee
+            
+            let delQuery = `DELETE FROM role WHERE roleId = ?`;
+
+            connection.query(delQuery, [delStr[1]], function (err, result, fields) {
+                if (err) throw err;
+                console.log (`Role id# ${delStr[1]} ${delStr[3]} has been ermoved.`);
+                //START OVER
+                administer_db();
+            });
+        });
+    });
+}
+
+const viewDepts = () => {
+    let deptSelectQuery = 'SELECT * from department ';
+    let deptSelectArr = [];
+    
+    connection.query(deptSelectQuery, function (err, result, fields) {
+        if (err) throw err;
+        Object.keys(result).forEach(function(key) {
+            let row = result[key];
+            //This array will be used for the next inquirer list
+            deptSelectArr.push(row);
+        });
+        console.table(deptSelectArr);
+    });
+}
+
+const addDept = () => {
+    inquirer
+    .prompt([
+        {
+            name: 'deptName',
+            type: 'input',
+            message: 'What is the name of your new Department?',
+            validate: async (input) => {
+                if (input.length === 0) 
+                {
+                    return 'You must enter something!';
+                }
+                return true;
+            }
+        },
+    ])
+    .then((answer) => {
+        let insertSQL = 'INSERT INTO department (deptName) ';
+        insertSQL += `VALUES  ('${answer.deptName}')`;
+        
+        connection.query(insertSQL, function (err, result, fields) {
+            if (err) throw err;
+            console.log (`${answer.deptName} has been added.`);
+            //START OVER
+            administer_db();
+        });
+    });
+}
+
+const deleteDept = () => {
+    //Query all employees for the user to select from
+    let roleDelQuery = 'SELECT * FROM department';
+    connection.query(roleDelQuery, function (err, result, fields) {
+        if (err) throw err;
+        let deptArray = [];
+        Object.keys(result).forEach(function(key) {
+            let row = result[key];
+            deptArray.push(`DeptId#: ${row.deptId} Name: ${row.deptName}`);
+        });
+
+        //Now determine which employee to delete
+        inquirer.prompt({
+            name: 'toDelete',
+            type: 'list',
+            message: 'Select a role to delete:',
+            choices: deptArray
+        })
+        .then((answer) => {
+            let roleString = answer.toDelete;
+            const delStr = roleString.split(" ");
+            //Now delete the selected employee
+            
+            let delQuery = `DELETE FROM department WHERE deptId = ?`;
+
+            connection.query(delQuery, [delStr[1]], function (err, result, fields) {
+                if (err) throw err;
+                console.log (`Dept id# ${delStr[1]} ${delStr[3]} has been ermoved.`);
+            });
+            //START OVER
+            administer_db();
         });
     });
 }
